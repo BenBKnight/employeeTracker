ef5be85ac16f5b89283add2e29d9555123052671
Ben K <ben.knight.brazil@hotmail.com>
2020-07-23 19:12:14 -0600

    Updates

---
+++ precache-manifest.0c7a33640c95269af7ee073d1d1a43f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4bf955d0e4f95def933dca12951791e",
+    "url": "/employeeTracker/index.html"
+  },
+  {
+    "revision": "31ebc6f963bc12ebf699",
+    "url": "/employeeTracker/static/css/main.49ebf199.chunk.css"
+  },
+  {
+    "revision": "b97284614587f20cb7a7",
+    "url": "/employeeTracker/static/js/2.a5c7f77b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/employeeTracker/static/js/2.a5c7f77b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31ebc6f963bc12ebf699",
+    "url": "/employeeTracker/static/js/main.237af328.chunk.js"
+  },
+  {
+    "revision": "5e4cddec16e71e23c456",
+    "url": "/employeeTracker/static/js/runtime-main.2badf6e5.js"
+  }
+]);
